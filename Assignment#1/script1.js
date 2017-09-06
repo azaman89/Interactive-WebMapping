5e8c38656620e3028c9fe79cac7bd921b1f299a3
azaman89 <azman8912@gmail.com>
2017-09-05 21:38:24 -0400

    assignment1 asif

---
+++ Assignment#1/script1.js
@@ -0,0 +1,29 @@
+/*
+ This is what a comment block looks like
+ only people that look at your code with see this
+ the computer will ignore it entierly
+*/
+
+// Assignment#1
+
+// arrays
+var firstArray = ["Biking", "Snorkeling","Eating", "Web Browsing","Skydiving"];
+console.log(firstArray[0]);
+console.log(firstArray[4]);
+
+// for loop through an array
+for (var i = 0; i < firstArray.length; i++) {
+	console.log("Array item number", i, firstArray[i]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
